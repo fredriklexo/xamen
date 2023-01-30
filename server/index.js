39a6f9ec69eaedf18a6dd4ecf75cab665eea2e49
Fredrik Lexö <fredrik@lexo.se>
2023-01-30 19:28:45 +0100

    test cookie

--- server/index.js
+++ server/index.js
@@ -22,8 +22,8 @@ db.once('open', () => console.log("Connected to Database"))
 
 
 //middelware
-// app.use(cors({ origin: true, credentials: true }));
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+// app.use(cors());
 app.use(cp())
 app.use('/stripe/webhook', stripeRouter, express.raw({ type: 'application/json' }), stripeRouter)
 app.use(express.json())
