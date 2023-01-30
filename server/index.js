73cee6cb043f106be41c6c150dae30df2fcdcd5c
Fredrik Lexö <fredrik@lexo.se>
2023-01-30 19:26:39 +0100

    cokki

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ db.once('open', () => console.log("Connected to Database"))
 
 //middelware
 // app.use(cors({ origin: true, credentials: true }));
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(cp())
 app.use('/stripe/webhook', stripeRouter, express.raw({ type: 'application/json' }), stripeRouter)
 app.use(express.json())
