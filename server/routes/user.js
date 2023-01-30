6c8d9222e82a934430caf9360f07a9dc9a94f507
Fredrik Lexö <fredrik@lexo.se>
2023-01-30 21:50:20 +0100

    cookie tessst

--- server/routes/user.js
+++ server/routes/user.js
@@ -123,6 +123,7 @@ router.post('/login', async (req, res) => {
                             secure: false,
                             maxAge: 60 * 60 * 24 * 3,
                             path: "/",
+                            domain: "xamen.vercel.app",
                             // httpOnly: true,
                             // secure: true,
                             // sameSite: "none",
